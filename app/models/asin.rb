b50d3275ba0705cd2fa2e03e8f5bb997cb465b4d
Will Ubuntu <will.hennessey735@gmail.com>
2018-09-13 19:57:29 +0100

    Adding common user-agent header and new approach, to prevent being blocked.

--- app/models/asin.rb
+++ app/models/asin.rb
@@ -1,12 +1,21 @@
 class Asin
   AMAZON_BASE_URL = 'https://amazon.com/dp/'
+  AGENT = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.85 Safari/537.36'
 
   def self.get_response_code(asin)
     url = "#{AMAZON_BASE_URL}#{asin}"
-    response = HTTP.get(url)
-    if response.code == 301
-      response = HTTP.get(response.headers['location'])
+    uri = URI(url)
+    result = get_response(uri)
+    if result.code.to_i == 301
+      uri = URI(result.header['location'])
+      result = get_response(uri)
     end
-    response.code
+    result.code.to_i
+  end
+
+  def self.get_response(uri)
+    req = Net::HTTP::Get.new(uri)
+    req['User-Agent'] = AGENT
+    Net::HTTP.start(uri.hostname, uri.port, :use_ssl => true) { |http| http.request(req) }
   end
 end
